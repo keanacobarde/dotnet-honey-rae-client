d8c1f562b00fa514cbb1170923c820fea58a7d9a
Keana Cobarde <keanacobarde@gmail.com>
2024-02-03 13:21:15 -0600

    getting employee details completed

--- src/components/employees/EmployeeDetails.jsx
+++ src/components/employees/EmployeeDetails.jsx
@@ -5,9 +5,41 @@ import { getEmployeeDetails } from "../../data/employeesData";
 
 function EmployeeDetails() {
   const { id } = useParams();
+  const [employeeTickets, setEmployeeTicket] = useState(null);
+
+  //add useEffect here to get the ticket details from the API
+  useEffect(() => {
+    getEmployeeDetails(id).then(setEmployeeTicket);
+  }, []);
+
+  if (!employeeTickets) {
+    return null;
+  }
+
   return (
-    <div>{id}</div>
+    <Table>
+   <thead>
+        <tr>
+          <th>Id</th>
+          <th>Description</th>
+          <th>Emergency?</th>
+          <th>Date Completed</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {employeeTickets.serviceTickets.map((t) => (
+          <tr key={`ticket-${t.id}`}>
+            <th scope="row">{t.id}</th>
+            <td>{t.description}</td>
+            <td>{t.emergency ? "yes" : "no"}</td>
+            <td>{t.dateCompleted?.split("T")[0] || "Incomplete"}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
+
 }
 
 export default EmployeeDetails
