58b5cdc5cdfa286f4461e46e9ba921b79778b036
Keana Cobarde <keanacobarde@gmail.com>
2024-02-03 10:01:15 -0600

    adding service tickets

--- src/components/employees/Employees.jsx
+++ src/components/employees/Employees.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 
 function Employees() {
-  return (
-    <div>Employees</div>
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+      getServiceTickets().then(setTickets);
+    }, []);
+  
+    return (
+      <Table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Specialty</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((e) => (
+            <tr key={`employee-${e.id}`}>
+              <th scope="row">{e.id}</th>
+              <td>{e.name}</td>
+              <td>{e.specialty}</td>
+              <td>
+                <Link to={`${e.id}`}>Service Tickets</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
   )
 }
 
