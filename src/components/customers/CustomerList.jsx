766292292ecda369f2cd4b6aeae786f9787e9fc3
Keana Cobarde <keanacobarde@gmail.com>
2024-02-04 18:05:20 -0600

    creating outline for POST promise

--- src/components/customers/CustomerList.jsx
+++ src/components/customers/CustomerList.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Table } from "reactstrap";
-import { Link } from "react-router-dom";
 import { getCustomers } from "../../data/customersData";
 
 
