6a7cd396b79193524f333784358256a7443caa1c
Keana Cobarde <keanacobarde@gmail.com>
2024-02-09 21:21:13 -0600

    getting the form corrected to follow acceptance criteria

--- src/components/tickets/CreateTicket.jsx
+++ src/components/tickets/CreateTicket.jsx
@@ -1,36 +1,102 @@
-import { Form, Row, Col, FormGroup, Label, Input} from "reactstrap";
+import { useEffect, useState } from "react";
+import { Form, Row, Col, FormGroup, Label, Input, Button} from "reactstrap";
+import { getEmployees } from "../../data/employeesData";
+import { getCustomers } from "../../data/customersData";
+
+// Setting Initial State
+const initialState = {
+  customerId: '',
+  employeeId: '',
+  description: '',
+  emergency: false,
+};
 
 export default function CreateTicket() {
+
+  const [formInput, setFormInput] = useState(initialState);
+  const [customers, setCustomers] = useState([]);
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    getEmployees().then((res) => setEmployees(res.map(employee => employee.id)))
+    getCustomers().then((res) => setCustomers(res.map((cus) => cus.id)));
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (<Form>
     <Row>
       <Col md={6}>
         <FormGroup>
-          <Label for="exampleEmail">
-            Email
+          <Label for="CustomerName">
+            Customer ID
           </Label>
           <Input
-            id="exampleEmail"
-            name="email"
-            placeholder="with a placeholder"
-            type="email"
-          />
+    className="mb-3"
+    type="select"
+    id="customerId"
+    name="text"
+    placeholder="CUstomer"
+    value={formInput.customerId}
+    onChange={handleChange}
+  >
+    {customers.map((cus) => <option key={cus}>{cus}</option>)}
+  </Input>
         </FormGroup>
       </Col>
     </Row>
     <Row>
       <Col md={6}>
         <FormGroup>
-          <Label for="exampleEmail">
-            Email
+          <Label for="Employee">
+            Employee Name
           </Label>
           <Input
-            id="exampleEmail"
-            name="email"
-            placeholder="with a placeholder"
-            type="email"
+    className="mb-3"
+    type="select"
+    id="employeeId"
+    name="text"
+    placeholder="Employee"
+    value={formInput.employeeId}
+    onChange={handleChange}
+  >
+    {employees.map((emp) => <option key={emp}>{emp}</option>)}
+  </Input>
+        </FormGroup>
+      </Col>
+    </Row>
+    <Row>
+      <Col md={6}>
+        <FormGroup>
+          <Label for="Description">
+            Description
+          </Label>
+          <Input
+            id="description"
+            name="text"
+            placeholder="Description of Issue"
+            type="text"
+            value={formInput.description}
+            onChange={handleChange}
           />
         </FormGroup>
       </Col>
     </Row>
+    <FormGroup check>
+    <Input type="checkbox" />
+    {' '}
+    <Label check>
+      Emergency?
+    </Label>
+  </FormGroup>
+  <Button type="submit">
+    Submit
+  </Button>
   </Form>);
 }
