cca5bff1c31df452e32ab6106f48a28241969374
Keana Cobarde <keanacobarde@gmail.com>
2024-02-10 19:55:31 -0600

    implementing delete button - controller logic not written yet

--- src/components/tickets/TicketsList.jsx
+++ src/components/tickets/TicketsList.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Table } from "reactstrap";
+import { Button, Table } from "reactstrap";
 import { getServiceTickets } from "../../data/serviceTicketsData";
 import { Link } from "react-router-dom";
 
@@ -9,6 +9,10 @@ export default function TicketsList() {
   useEffect(() => {
     getServiceTickets().then(setTickets);
   }, []);
+
+  const handleDelete = () => {
+    console.warn('Delete?');
+  }
 
   return (
     <Table>
@@ -30,6 +34,9 @@ export default function TicketsList() {
             <td>{t.dateCompleted?.split("T")[0] || "Incomplete"}</td>
             <td>
               <Link to={`${t.id}`}>Details</Link>
+            </td>
+            <td onClick={handleDelete}>
+              <Button color="danger"> Delete </Button>
             </td>
           </tr>
         ))}
